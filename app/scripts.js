a31170b7abd17881febe0e900e2574a7d9963da2
Dmitr0k <dkukhno@gmail.com>
2018-04-22 01:10:44 +0300

    Added some logic
    + added touchable buttons
    + added cost logic

---
+++ app/scripts.js
@@ -0,0 +1,50 @@
+window.onload = function () {
+    var app = new Vue ({
+        el: ".shop-app",
+        data: {
+            cart: [],
+            cost: "5 - 7",
+            sizes: {
+                s: false,
+                m: false,
+                l: false,
+                xl: false,
+                xxl: false,
+                xxxl: false
+            },
+            genders: {
+                male: false,
+                female: false
+            },
+            types: {
+                regular: false,
+                warm: false
+            }
+        },
+        methods: {
+            set_current_size: function (size) {
+                for (sz in this.sizes) {
+                    this.sizes[sz] = false;
+                }
+                this.sizes[size] = true;
+            },
+            set_current_gender: function (gender) {
+                for (gen in this.genders) {
+                    this.genders[gen] = false;
+                }
+                this.genders[gender] = true;
+            },
+            set_current_type: function (type) {
+                for (tp in this.types) {
+                    this.types[tp] = false;
+                }
+                this.types[type] = true;
+                if (this.types.regular && !this.types.warm) {
+                    this.cost = "5"
+                } else if (!this.types.regular && this.types.warm) {
+                    this.cost = "7"
+                }
+            }
+        }
+    })
+}
